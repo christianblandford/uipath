82251c70630afc18a974c104ba56df56175fa149
Christian Blandford <christianblandford@me.com>
2025-02-11 19:32:58 -0800

    Bump version number to 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="uipath",
-    version="1.0.0",
+    version="1.1.1",
     author="Christian Blandford",
     author_email="christianblandford@me.com",
     description="A Python SDK for UiPath",
